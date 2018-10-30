f5c8b251aa4cb5f85edb222b9e41d66466782f27
ChoYG <whdlgp@gmail.com>
2018-10-30 19:12:42 +0900

    Add gradient method for edge weight

--- graphcut_test.m
+++ graphcut_test.m
@@ -1,48 +1,113 @@
 clear;
 clc;
 
+%determin weight type
+% 1 : weight with RGB pixel
+% 2 : weight with RGB pixel, use gradiant method
+weight_type = 1;
+
 %original images and mask
 % RGB
 im1 = imread('im1.jpg');
 im2 = imread('im2.jpg');
 
 [height, width, channel] = size(im1);
+
 vertice_size = height * width; % number of vertice without souce and sink
 
 %number of vertex pair(number of edge)
 %X axis edge, Y axis edge, source/sink edge
-number_of_pair = ((width - 1) * height) + (width * (height - 1)) + (2 * height);
+number_of_pair = (((width - 1) * height) + (width * (height - 1))) + (2 * height);
 s = zeros(1,number_of_pair); % image graph's edge and source, sink edge
 t = zeros(1,number_of_pair); % image graph's edge and source, sink edge
 weight = zeros(1,number_of_pair); % image graph's edge and source, sink edge
 
 index = 1;
 
-% draw graph with image pixels
-for vertex = 1:vertice_size
-    disp('vertice number :');
-    disp(vertex);
-    disp('percentage :');
-    disp(100*(vertex/vertice_size));
-    
-    x = mod(vertex-1, width) + 1;
-    y = fix((vertex-1) / width) + 1;
-    % X axis weight
-    if mod(vertex, width) ~= 0 % Except right-side vertex
-        s(index) = vertex;
-        t(index) = vertex+1;
-        weight(index) = normL2(im1(y,x,:), im2(y,x,:)) ...
-                        + normL2(im1(y,x+1,:), im2(y,x+1,:));
-        index = index+1;
+% 1 : weight with RGB pixel
+if weight_type==1
+    % draw graph with image pixels
+    for vertex = 1:vertice_size
+        disp('vertice number :');
+        disp(vertex);
+        disp('percentage :');
+        disp(100*(vertex/vertice_size));
+
+        x = mod(vertex-1, width) + 1;
+        y = fix((vertex-1) / width) + 1;
+        % X axis weight
+        if mod(vertex, width) ~= 0 % Except right-side vertex
+            s(index) = vertex;
+            t(index) = vertex+1;
+            weight(index) = (normL2(im1(y,x,:), im2(y,x,:)) ...
+                            + normL2(im1(y,x+1,:), im2(y,x+1,:)));
+            index = index+1;
+        end
+
+        % Y axis weight
+        if vertex <= width * (height - 1) % Except bottom-side vertex
+            s(index) = vertex;
+            t(index) = vertex+width;
+            weight(index) = (normL2(im1(y,x,:), im2(y,x,:)) ...
+                            + normL2(im1(y+1,x,:), im2(y+1,x,:)));
+            index = index+1;
+        end
     end
-    
-    % Y axis weight
-    if vertex <= width * (height - 1) % Except bottom-side vertex
-        s(index) = vertex;
-        t(index) = vertex+width;
-        weight(index) = normL2(im1(y,x,:), im2(y,x,:)) ...
-                        + normL2(im1(y+1,x,:), im2(y+1,x,:));
-        index = index+1;
+end
+
+% 2 : weight with RGB pixel, use gradiant method
+if weight_type==2
+    %sobel filtered images
+    sobel_x = [-1 0 1;-2 0 2;-1 0 1];
+    sobel_y = [-1 -2 -1;0 0 0;1 2 1];
+
+    im1_dx_3ch = imfilter(im1, sobel_x);
+    im1_dy_3ch = imfilter(im1, sobel_y);
+    im2_dx_3ch = imfilter(im2, sobel_x);
+    im2_dy_3ch = imfilter(im2, sobel_y);
+
+    im1_dx_1ch = zeros(height, width);
+    im1_dy_1ch = zeros(height, width);
+    im2_dx_1ch = zeros(height, width);
+    im2_dy_1ch = zeros(height, width);
+
+    for x = 1:width
+        for y = 1:height
+            im1_dx_1ch(y,x) = normL2(im1_dx_3ch(y,x,:));
+            im1_dy_1ch(y,x) = normL2(im1_dy_3ch(y,x,:));
+            im2_dx_1ch(y,x) = normL2(im2_dx_3ch(y,x,:));
+            im2_dy_1ch(y,x) = normL2(im2_dy_3ch(y,x,:));
+        end
+    end
+
+    % draw graph with image pixels
+    for vertex = 1:vertice_size
+        disp('vertice number :');
+        disp(vertex);
+        disp('percentage :');
+        disp(100*(vertex/vertice_size));
+
+        x = mod(vertex-1, width) + 1;
+        y = fix((vertex-1) / width) + 1;
+        % X axis weight
+        if mod(vertex, width) ~= 0 % Except right-side vertex
+            grad = im1_dx_1ch(y,x) + im1_dx_1ch(y,x+1) + im2_dx_1ch(y,x) + im2_dx_1ch(y,x+1);
+            s(index) = vertex;
+            t(index) = vertex+1;
+            weight(index) = (normL2(im1(y,x,:), im2(y,x,:)) ...
+                            + normL2(im1(y,x+1,:), im2(y,x+1,:)))/(grad+1);
+            index = index+1;
+        end
+
+        % Y axis weight
+        if vertex <= width * (height - 1) % Except bottom-side vertex
+            grad = im1_dy_1ch(y,x) + im1_dy_1ch(y+1,x) + im2_dy_1ch(y,x) + im2_dy_1ch(y+1,x);
+            s(index) = vertex;
+            t(index) = vertex+width;
+            weight(index) = (normL2(im1(y,x,:), im2(y,x,:)) ...
+                            + normL2(im1(y+1,x,:), im2(y+1,x,:)))/(grad+1);
+            index = index+1;
+        end
     end
 end
 
--- normL2.m
+++ normL2.m
@@ -1,5 +1,15 @@
-function n = normL2(A, B)
+function n = normL2(varargin)
+    if nargin==2
+    A=varargin{1};
+    B=varargin{2};
     tmp = double(A) - double(B);
     tmp = tmp.^2;
     n = sum(tmp);
+    end
+    
+    if nargin==1
+    A=varargin{1};
+    tmp = double(A).^2;
+    n = sum(tmp);
+    end
 end
